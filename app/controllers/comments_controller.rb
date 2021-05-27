627aec76b74db49b30002e5db667504f92111029
Luis Castillo <mr.luiscastillo@gmail.com>
2021-05-27 17:55:17 -0400

    in comment controller create,show,edit and create index view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,12 +8,20 @@ class CommentsController < ApplicationController
     end
 
     def create
+        @comment = current_user.comments.build(comment_params)
+        if @comment.save
+            redirect_to comments_path
+        else
+            render :new
+        end
     end
 
     def show
+        @comment = Comment.find_by(id: params[:id])
     end
 
     def edit
+        @comment = Comment.find_by(id: params[:id])
     end
 
     def update
