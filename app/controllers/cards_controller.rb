b5f410f4d58ba0386802903539fe1957700c0312
Luis Castillo <mr.luiscastillo@gmail.com>
2021-05-26 21:40:56 -0400

    add new card form

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,3 +1,22 @@
 class CardsController < ApplicationController
     before_action :if_not_logged_in_redirect
+
+    def new 
+        @card = Card.new
+    end
+
+    def create
+       @card = current_user.cards.build(card_params)
+       if @card.save
+            redirect_to cards_path
+       else
+            render :new
+       end
+    end
+
+    private
+
+    def card_params
+        params.require(:card).permit(:player_name, :team_name, :year)
+    end
 end
