e62038aaf7f759b98815a0300cb804c327d00c83
Luis Castillo <mr.luiscastillo@gmail.com>
2021-06-02 23:39:34 -0400

    many to many in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
     has_secure_password
     has_many :cards
     has_many :comments
-    has_many :descriptions, through: :cards
+    has_many :commented_cards, through: :comments, source: :card
+    #has_many :descriptions, through: :cards
 
      validates :username, :email, presence: true
 end
